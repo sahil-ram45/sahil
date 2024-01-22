b2c663d1498f102042edc8634f068e19351fbbb6
sahil-ram45 <157342047+sahil-ram45@users.noreply.github.com>
2024-01-22 10:59:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p> <strong>Roll No:</strong> 2211CS010509</p>
+        </div>
+        <div><p> <strong>Name</strong> Sahil</p></div>
+        <div><p> <strong>CGPA</strong> 9.25</p></div>
+        
+      </div>
+    
+  );
+}
+
+export default App;
